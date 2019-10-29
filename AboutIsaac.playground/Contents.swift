f8328545892d18bdfee339de63785a640ad52ed6
Isaac Grove <isaacgrove333@gmail.com>
2019-10-29 13:52:34 -0400

    added my favorite food

--- AboutIsaac.playground/Contents.swift
+++ AboutIsaac.playground/Contents.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 var str = "Hello, playground"
-
+//this playground was to learn how to use git and github
 let myName = "Isaac"
 let hobbies = "I like to work out, be outdoors, and read."
 
+let favoriteFood = "Pizza"
